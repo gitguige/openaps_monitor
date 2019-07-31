ce0c7e876138632263a18ae6a59b091d6349def6
guige <guige2019@gmail.com>
2019-07-30 21:34:50 -0400

    edit test.sh

--- Reseult/test.sh
+++ Reseult/test.sh
@@ -1,5 +1,11 @@
-python3 bgrisk_monitor_constrain_baseline.py /home/uva-dsa/Research/Medical/openaps_monitor/Reseult/simulationCollectdata_hardware/ /home/uva-dsa/Research/Medical/openaps_monitor/myopenaps/fault_library_monitor/scenario_  summary_monitor_hardware_baseline.csv
-python3 bgrisk_monitor_constrain.py /home/uva-dsa/Research/Medical/openaps_monitor/Reseult/simulationCollectdata_hardware/ /home/uva-dsa/Research/Medical/openaps_monitor/myopenaps/fault_library_monitor/scenario_  summary_monitor_hardware.csv
-python3 bgrisk_monitor_constrain_faultfree_baseline.py /home/uva-dsa/Research/Medical/openaps_monitor/Reseult/faultFreeSimulation_data/  summary_faultfree_baseline.csv
-python3 bgrisk_monitor_constrain_faultfree.py /home/uva-dsa/Research/Medical/openaps_monitor/Reseult/faultFreeSimulation_data/  summary_faultfree.csv
+## use pwd command ##
+_mydir="$(pwd)"
+# faultlib = "$(pwd)/../myopenaps/fault_library_monitor/scenario_"
+
+echo $_mydir
+# echo $(pwd)/../myopenaps/fault_library_monitor/scenario_
+python3 bgrisk_monitor_constrain_baseline.py $(pwd)/simulationCollectdata_hardware/ $(pwd)/../myopenaps/fault_library_monitor/scenario_  summary_monitor_hardware_baseline.csv
+python3 bgrisk_monitor_constrain.py $(pwd)/simulationCollectdata_hardware/ $(pwd)/../myopenaps/fault_library_monitor/scenario_  summary_monitor_hardware.csv
+python3 bgrisk_monitor_constrain_faultfree_baseline.py $(pwd)/faultFreeSimulation_data/  summary_faultfree_baseline.csv
+python3 bgrisk_monitor_constrain_faultfree.py $(pwd)/faultFreeSimulation_data/  summary_faultfree.csv
 python gen_table_from_result.py
