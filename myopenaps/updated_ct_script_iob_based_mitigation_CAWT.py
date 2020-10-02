54c649d5dfda26b9b4377bdc25cc9703193dcde7
gitguige <guige2019@gmail.com>
2020-10-01 22:13:07 -0400

    edit CAWT mitigation script

--- myopenaps/updated_ct_script_iob_based_mitigation_CAWT.py
+++ myopenaps/updated_ct_script_iob_based_mitigation_CAWT.py
@@ -231,17 +231,35 @@ for _ in range(iteration_num):
           mitigate_H1_flag = False #reset hazard flag
         loaded_suggested_data["rate"] = 0
 
+        loaded_suggested_data["fault"] = "yes"
+        loaded_suggested_data["fault_reason"] = sub_alert_msg+"_Mitigation" 
+        print("\n***************************************")
+        print("********** Unsafe Action !!!!! *************")    
+
       elif mitigate_H2_flag == True: 
         if loaded_suggested_data["rate"]  > prev_rate or glucose<bg_target+40:#if fault is removed stop mitigation
           mitigate_H2_flag = False
-          loaded_suggested_data["rate"]  = 2.1
+        loaded_suggested_data["rate"]  = 2.1
+
+        loaded_suggested_data["fault"] = "yes"
+        loaded_suggested_data["fault_reason"] = sub_alert_msg+"_Mitigation" 
+        print("\n***************************************")
+        print("********** Unsafe Action !!!!! *************")    
 
         #==========mitigation code####++==============
     elif Monitor ==1: #MPC
       if glucose <70 :#H1hazard
         loaded_suggested_data["rate"] = 0
+        loaded_suggested_data["fault"] = "yes"
+        loaded_suggested_data["fault_reason"] = sub_alert_msg+"_Mitigation" 
+        print("\n***************************************")
+        print("********** Unsafe Action !!!!! *************")    
       elif glucose>180:#H2hazard
         loaded_suggested_data["rate"]  = 2.1
+        loaded_suggested_data["fault"] = "yes"
+        loaded_suggested_data["fault_reason"] = sub_alert_msg+"_Mitigation" 
+        print("\n***************************************")
+        print("********** Unsafe Action !!!!! *************")    
 
 
                     
